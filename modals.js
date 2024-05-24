88b14bd047a0f2fce38725411b013f7f0a66de07
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-25 01:51:33 +0700

    Update modals.js

--- modals.js
+++ modals.js
@@ -1,18 +1,11 @@
-var btns = document.querySelectorAll(".popup");
-for (let b of btns) {
-  b.onclick = () => {
-    let modal = b.parentElement.querySelector(".modal")
-    let span = modal.querySelector(".close");
-    modal.style.display = "block";
-    
-    span.onclick = () => {
-      modal.style.display = "none";
-    }
-    
-    window.onclick = (event) => {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-  }
-}
+const openModal = document.querySelector("[data-open-modal]"),
+      modal = document.querySelector("[data-modal]"),
+      closeModal = document.querySelector("[data-close-modal]");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
