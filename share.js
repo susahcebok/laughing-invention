85e3937ebe43a58f6d63c86d9d5a2428b5aca984
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-24 14:33:47 +0700

    Create share.js

---
+++ share.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("files");
+const output = document.getElementById("output");
+
+document.getElementById("share").addEventListener("click", async () => {
+  const files = input.files;
+
+  if (files.length === 0) {
+    output.textContent = "No files selected.";
+    return;
+  }
+
+  // feature detecting navigator.canShare() also implies
+  // the same for the navigator.share()
+  if (!navigator.canShare) {
+    output.textContent = `Your browser doesn't support the Web Share API.`;
+    return;
+  }
+
+  if (navigator.canShare({ files })) {
+    try {
+      await navigator.share({
+        files,
+        title: "Profile Pic Generator",
+        text: "Profile Pic Generator",
+      });
+      output.textContent = "Shared!";
+    } catch (error) {
+      output.textContent = `Error: ${error.message}`;
+    }
+  } else {
+    output.textContent = `Your system doesn't support sharing these files.`;
+  }
+});
