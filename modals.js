3d20674c657f1b2d7264bc3d98922270d07feb55
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-25 01:20:01 +0700

    Create modals.js

---
+++ modals.js
@@ -0,0 +1,18 @@
+var btns = document.querySelectorAll("btn");
+for (let b of btns) {
+  b.onclick = () => {
+    let modal = b.parentElement.querySelector(".modal")
+    let span = modal.querySelector(".close");
+    modal.style.display = "block";
+    
+    span.onclick = () => {
+      modal.style.display = "none";
+    }
+    
+    window.onclick = (event) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
+}
