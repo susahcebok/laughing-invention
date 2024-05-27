0c5598bcde8df0f8638300e3a86a9443c43c27c0
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-27 12:28:01 +0700

    Update modals.js

--- js/modals.js
+++ js/modals.js
@@ -1,7 +1,7 @@
 (function($) {
 "use strict";
 $(document).ready(function() {
-$('.modalbtn').on('click', function() {
+$('.btn').on('click', function() {
 $('body').addClass("modal-open");
 });
 $('.close-modal').on('click', function() {
