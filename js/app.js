f930e779a9674ef6f373f69ba1da489097c49998
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-05-28 11:02:31 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function(e) {
+      $('#fileInput').css('display', 'none');
       $("#preview").attr("src", e.target.result);
     };
     reader.readAsDataURL(input.files[0]);
